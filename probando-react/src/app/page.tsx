ab8fb6a13dc33beda980fb058e3231a7179aee06
Martín Capuano <capuano.martin@outlook.com>
2025-10-15 11:22:12 -0300

    primer hola mundo en react

---
+++ probando-react/src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1 style={{
+          textAlign: "center",
+          fontSize: "50px",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          Hola mundo
+        </h1>
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  );
+}
